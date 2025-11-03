1988f822305ac4dc725218e6c4252af822c4d974
cocojisoo <yjs1019a@gmail.com>
2025-11-03 23:00:57 +0900

    feat(service) LottoGenerator 메서드 추가

--- src/main/java/lotto/service/LottoGenerator.java
+++ src/main/java/lotto/service/LottoGenerator.java
@@ -1,6 +1,19 @@
 package lotto.service;
 
+import camp.nextstep.edu.missionutils.Randoms;
+import java.util.ArrayList;
+import java.util.List;
+import lotto.domain.LottoTicket;
+
 public class LottoGenerator {
+    public List<LottoTicket> generate(int count) {
+        List<LottoTicket> tickets = new ArrayList<>();
+        for (int i = 0; i < count; i++) {
+            List<Integer> numbers = Randoms.pickUniqueNumbersInRange(1, 45, 6);
+            tickets.add(LottoTicket.ofIntegers(numbers));
+        }
+        return tickets;
+    }
 }
 
 
