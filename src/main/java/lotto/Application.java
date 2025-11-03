70963c1009cccd8486ab5fccf4a936493d6124a8
cocojisoo <yjs1019a@gmail.com>
2025-11-03 23:29:17 +0900

    feat(application): AppConfig 추가

--- src/main/java/lotto/Application.java
+++ src/main/java/lotto/Application.java
@@ -1,7 +1,11 @@
 package lotto;
+import lotto.application.AppConfig;
+import lotto.controller.LottoController;
 
 public class Application {
     public static void main(String[] args) {
-        // TODO: 프로그램 구현
+        AppConfig config = new AppConfig();
+        LottoController controller = config.lottoController();
+        controller.run();
     }
 }
--- src/main/java/lotto/application/AppConfig.java
+++ src/main/java/lotto/application/AppConfig.java
@@ -1,6 +1,21 @@
 package lotto.application;
 
+import lotto.controller.LottoController;
+import lotto.service.LottoGenerator;
+import lotto.service.LottoJudge;
+import lotto.service.ProfitCalculator;
+import lotto.view.InputView;
+import lotto.view.OutputView;
+
 public class AppConfig {
+    public LottoController lottoController() {
+        InputView inputView = new InputView();
+        OutputView outputView = new OutputView();
+        LottoGenerator generator = new LottoGenerator();
+        LottoJudge judge = new LottoJudge();
+        ProfitCalculator profitCalculator = new ProfitCalculator();
+        return new LottoController(inputView, outputView, generator, judge, profitCalculator);
+    }
 }
 
 
