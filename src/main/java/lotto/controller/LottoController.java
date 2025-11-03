46a8a79a0798c8a7ef36c47b7c2a7b16777c1987
cocojisoo <yjs1019a@gmail.com>
2025-11-03 23:23:50 +0900

    feat(controller): LottoController 의존성 추가 및 흐름제어 추가

--- src/main/java/lotto/controller/LottoController.java
+++ src/main/java/lotto/controller/LottoController.java
@@ -1,6 +1,83 @@
 package lotto.controller;
 
-public class GameController {
+import java.util.List;
+
+import lotto.domain.LottoNumber;
+import lotto.domain.LottoTicket;
+import lotto.domain.Money;
+import lotto.domain.Result;
+import lotto.domain.WinningNumbers;
+import lotto.service.LottoGenerator;
+import lotto.service.LottoJudge;
+import lotto.service.ProfitCalculator;
+import lotto.view.InputView;
+import lotto.view.OutputView;
+
+public class LottoController {
+    private final InputView inputView;
+    private final OutputView outputView;
+    private final LottoGenerator generator;
+    private final LottoJudge judge;
+    private final ProfitCalculator profitCalculator;
+
+    public LottoController(InputView inputView, OutputView outputView, LottoGenerator generator,
+                           LottoJudge judge, ProfitCalculator profitCalculator) {
+        this.inputView = inputView;
+        this.outputView = outputView;
+        this.generator = generator;
+        this.judge = judge;
+        this.profitCalculator = profitCalculator;
+    }
+
+    public void run() {
+        outputView.printPurchasePrompt();
+        Money purchase = readPurchaseMoneyWithRetry();
+
+        int ticketCount = (int) (purchase.getAmount() / 1_000);
+        List<LottoTicket> tickets = generator.generate(ticketCount);
+        outputView.printPurchaseCount(ticketCount);
+        outputView.printTickets(tickets);
+
+        outputView.printWinningPrompt();
+        List<LottoNumber> winningNumbers = readWinningNumbersWithRetry();
+        outputView.printBonusPrompt();
+        LottoNumber bonus = readBonusNumberWithRetry(winningNumbers);
+
+        WinningNumbers winning = WinningNumbers.of(winningNumbers, bonus);
+        Result result = judge.judge(tickets, winning);
+        String rate = profitCalculator.calculateRate(result, purchase);
+        outputView.printStatistics(result, rate);
+    }
+
+    private Money readPurchaseMoneyWithRetry() {
+        while (true) {
+            try {
+                return inputView.readPurchaseMoney();
+            } catch (IllegalArgumentException e) {
+                outputView.printError(e.getMessage());
+            }
+        }
+    }
+
+    private List<LottoNumber> readWinningNumbersWithRetry() {
+        while (true) {
+            try {
+                return inputView.readWinningNumbers();
+            } catch (IllegalArgumentException e) {
+                outputView.printError(e.getMessage());
+            }
+        }
+    }
+
+    private LottoNumber readBonusNumberWithRetry(List<LottoNumber> winningNumbers) {
+        while (true) {
+            try {
+                return inputView.readBonusNumber(winningNumbers);
+            } catch (IllegalArgumentException e) {
+                outputView.printError(e.getMessage());
+            }
+        }
+    }
 }
 
 
