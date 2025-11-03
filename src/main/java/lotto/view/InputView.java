c8ad206d345e02fa3fa20bbb8a0ee75adedd10bf
cocojisoo <yjs1019a@gmail.com>
2025-11-03 23:15:40 +0900

    feat(view): InputView 추가

--- src/main/java/lotto/view/InputView.java
+++ src/main/java/lotto/view/InputView.java
@@ -1,6 +1,26 @@
 package lotto.view;
 
+import camp.nextstep.edu.missionutils.Console;
+import java.util.List;
+import lotto.domain.LottoNumber;
+import lotto.domain.Money;
+import lotto.util.Parser;
+
 public class InputView {
+    public Money readPurchaseMoney() {
+        String line = Console.readLine();
+        return Parser.parsePurchaseMoney(line);
+    }
+
+    public List<LottoNumber> readWinningNumbers() {
+        String line = Console.readLine();
+        return Parser.parseCsvWinningNumbers(line);
+    }
+
+    public LottoNumber readBonusNumber(List<LottoNumber> winningNumbers) {
+        String line = Console.readLine();
+        return Parser.parseBonusNumber(line, winningNumbers);
+    }
 }
 
 
