2db3f6a19b38575609792f04040025342540d4f9
cocojisoo <yjs1019a@gmail.com>
2025-11-03 23:03:21 +0900

    feat(service): ProfitCalculator 메서드 추가

--- src/main/java/lotto/service/ProfitCalculator.java
+++ src/main/java/lotto/service/ProfitCalculator.java
@@ -1,6 +1,13 @@
 package lotto.service;
 
+import lotto.domain.Money;
+import lotto.domain.Result;
+
 public class ProfitCalculator {
+    public String calculateRate(Result result, Money purchase) {
+        Money totalPrize = result.getTotalPrize();
+        return totalPrize.formatRateAgainst(purchase);
+    }
 }
 
 
